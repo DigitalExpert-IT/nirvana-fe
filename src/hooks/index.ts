82a63f5af984492f9ef9c004af383beb45682ae5
Y0SH4 <yosa.agung13@gmail.com>
2024-10-17 14:04:04 +0700

    fix error

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from "./contract";
 export * from "./useScreen";
 export * from "./useAsyncCall";
-export * from "./useCurrentChainId";
 export * from "./useSwapContract";
 export * from "./useUSDTContract";
--- src/hooks/useCurrentChainId.ts
+++
@@ -1,2 +0,0 @@
-export const CURRENT_CHAIN_ID = (process.env.NEXT_PUBLIC_CHAIN_ID ||
-  "0x89") as "0x89";
